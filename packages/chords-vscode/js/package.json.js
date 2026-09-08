4dc051c5ee7c000d09199b0a7fb31379d8834632
Leon Si <leon@leonsilicon.com>
2026-09-08 13:52:56 -0400

    pack vscode

--- packages/chords-vscode/js/package.json.js
+++ packages/chords-vscode/js/package.json.js
@@ -1,20 +1,34 @@
 //#region package.json
 var name = "@keychord/chords-vscode";
 var version = "0.0.0";
+var repository = {
+  type: "git",
+  url: "https://github.com/KeyChord/chords-vscode",
+};
 var type = "module";
 var dependencies = { "nano-spawn-compat": "latest" };
 var devDependencies = {
-	"@keychord/config": "catalog:",
-	"@keychord/tsconfig": "catalog:"
+  "@keychord/config": "catalog:",
+  "@keychord/tsconfig": "catalog:",
 };
 var packageManager = "pnpm@10.33.0";
 var package_default = {
-	name,
-	version,
-	type,
-	dependencies,
-	devDependencies,
-	packageManager
+  name,
+  version,
+  repository,
+  type,
+  dependencies,
+  devDependencies,
+  packageManager,
 };
 //#endregion
-export { package_default as default, dependencies, devDependencies, name, packageManager, type, version };
+export {
+  package_default as default,
+  dependencies,
+  devDependencies,
+  name,
+  packageManager,
+  repository,
+  type,
+  version,
+};
--- packages/chords-vscode/src/js/vscode.ts
+++ packages/chords-vscode/src/js/vscode.ts
@@ -4,29 +4,31 @@ import path from "path";
 import { tap } from "chord";
 import spawn from "nano-spawn-compat";
 
-// TODO: make this work for Cursor
-const uid = await spawn("id", ["-u"]);
+export default async function buildAction() {
+  // TODO: make this work for Cursor
+  const uid = await spawn("id", ["-u"]);
 
-export default function vscode(cmd: string) {
-  const tmp = process.env.TMPDIR ?? "/tmp";
-  const dir = path.join(tmp, `vscode-command-server-${uid.stdout}`);
+  return function action(cmd: string) {
+    const tmp = process.env.TMPDIR ?? "/tmp";
+    const dir = path.join(tmp, `vscode-command-server-${uid.stdout}`);
 
-  if (!exists(dir)) {
-    return false;
-  }
+    if (!exists(dir)) {
+      return false;
+    }
 
-  const requestPath = path.join(dir, "request.json");
-  const responsePath = path.join(dir, "response.json");
+    const requestPath = path.join(dir, "request.json");
+    const responsePath = path.join(dir, "response.json");
 
-  const payload = JSON.stringify({
-    commandId: cmd,
-    args: [],
-  });
+    const payload = JSON.stringify({
+      commandId: cmd,
+      args: [],
+    });
 
-  fs.writeFileSync(requestPath, payload);
-  // We need to remove the previous response.json before continuing
-  fs.rmSync(responsePath, { force: true });
-  tap("cmd+shift+f17");
+    fs.writeFileSync(requestPath, payload);
+    // We need to remove the previous response.json before continuing
+    fs.rmSync(responsePath, { force: true });
+    tap("cmd+shift+f17");
 
-  return true;
+    return true;
+  };
 }
